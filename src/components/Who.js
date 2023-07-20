23976089ade28fb91bf70f6041ce05ae02c47aab
Olena Reukova <kronverk01@gmail.com>
2023-07-20 22:27:17 +0100

    Who component css
    it is going to be a section about me

--- src/components/Who.js
+++ src/components/Who.js
@@ -1,16 +1,85 @@
-import React from 'react';
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const WhatIDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: white;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: grey;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 function Who() {
   return (
-    <Section >
-    <p>Who</p>
-   </Section>
+    <Section>
+      <Container>
+        <Left>{/*3d model*/}</Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatIDo>
+            <Line src="./img/line.png" />
+            <Subtitle>Who I am</Subtitle>
+          </WhatIDo>
+          <Desc>
+            I am a Full Stack Developer with a passion for developing.
+          </Desc>
+          <Button>See my projects </Button>
+        </Right>
+      </Container>
+    </Section>
   );
 }
 
