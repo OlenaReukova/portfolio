c950c9ed6762d015db5723ac63cd438e19f796e1
Olena Reukova <kronverk01@gmail.com>
2023-07-21 10:45:30 +0100

    Works component css

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,16 +1,93 @@
-import React from 'react';
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
+
+const data = [
+  "CYF progress tracker",
+  "Artificial Intelligence",
+  "Hotel booking website",
+  "TV project",
+  "Flowers shop",
+];
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ListItem = styled.li`
+  font-size: 60px;
+  font-weight: bold;
+  cursor: pointer;
+  color: white;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+  ::after {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #f45050;
+    width: 0px;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    ::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
 `;
 
 function Works() {
   return (
-    <Section >
-    <p>Works</p>
-   </Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} text={item}>
+                {item}
+              </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
   );
 }
 
