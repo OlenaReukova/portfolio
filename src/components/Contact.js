67d1a72c2bc4c5e1a1aeaa89731145b7980b5ab9
Olena Reukova <kronverk01@gmail.com>
2023-07-21 12:38:11 +0100

    Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,80 @@
-import React from 'react';
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
 `;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: white;
+  border: none;
+  border-radius: 5px;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  background-color: #f45050;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 function Contact() {
   return (
-    <Section >
-    <p>Contact</p>
-   </Section>
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Me</Title>
+            <Input placeholder="Name"></Input>
+            <Input placeholder="Email"></Input>
+            <TextArea placeholder="Write your message" rows={10}></TextArea>
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
   );
 }
 
